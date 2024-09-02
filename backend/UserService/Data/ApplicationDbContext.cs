a8e2d3045f9b70baaa74269992a56ddda492a9ff
unknown <chandrunandish@gmail.com>
2024-09-02 11:09:43 +0530

    added

---
+++ backend/UserService/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using UserService.Models;
+
+namespace UserService.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
